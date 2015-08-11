3be7f7e05f23952be3e1bfc773443b64347954a4
Donald Disha <dondished@gmail.com>
2015-08-11 17:50:00 -0400

    added the star of the show, and gave him arrow key movement

--- mvc/controllers/homepage.js
+++ mvc/controllers/homepage.js
@@ -2,7 +2,8 @@ module.exports = function(app) {
   app.route('/').get(function(req, res) {
     var model = require('models/global')(req, res);
     model.content.pageTitle = '{content.appTitle} - World Map';
-    model.teddyPath = '/images/worldMap.jpg';
+    model.teddyPath1 = '/images/worldMap.jpg';
+    model.teddyPath2 = '/images/hero.gif';
     res.render('homepage', model);
   });
 };
--- statics/js/main.js
+++ statics/js/main.js
@@ -31,6 +31,38 @@ newWorldProject.moveObject = function() {
   document.addEventListener('mousedown', mouseDown);
 };
 
+newWorldProject.movementKeys = function() {
+  var hero = document.getElementById('hero');
+    
+  document.addEventListener('keydown', function(e) {
+    var heroLeftPos = parseInt(window.getComputedStyle(hero).getPropertyValue('margin-left')),
+        heroTopPos = parseInt(window.getComputedStyle(hero).getPropertyValue('margin-top'));
+    
+    switch(e.keyCode) {
+      case 37: // left arrow
+        console.log('left');
+        hero.style['margin-left'] = (heroLeftPos - 32) + 'px';
+        break;
+      case 39: // right arrow
+        console.log('right');
+        hero.style['margin-left'] = (heroLeftPos + 32) + 'px';
+        break;
+      case 38: // up arrow        
+        console.log('up');
+        hero.style['margin-top'] = (heroTopPos - 32) + 'px';
+        break;
+      case 40: // down arrow
+        console.log('down');
+        hero.style['margin-top'] = (heroTopPos + 32) + 'px';
+        break;
+    }
+    
+    console.log(heroLeftPos);
+    console.log(heroTopPos);
+  });
+};
+
 window.addEventListener('load', function() {
+  newWorldProject.movementKeys();
   newWorldProject.moveObject();
 });
