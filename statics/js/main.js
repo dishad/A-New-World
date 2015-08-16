39f1b83e7c36d60dee42891288bf911e8d79e330
Donald Disha <dondished@gmail.com>
2015-08-16 17:12:31 -0400

    Added more weapons, their damage, cost, and whether or not they can be bought

--- statics/js/main.js
+++ statics/js/main.js
@@ -32,6 +32,57 @@ newWorldProject.zoneOne = [
     damageMax: 2
   }
 ];
+
+newWorldProject.combatWeapons = [
+  {
+    name: 'Fists of Destiny',
+    src: '',
+    damageMin: 0,
+    damageMax: 2,
+    cost: ''
+  },
+  {
+    name: 'Wooden Sword',
+    src: '../images/woodSword.png',
+    damageMin: 1,
+    damageMax: 3,
+    cost: 10
+  },
+  {
+    name: 'Iron Sword',
+    src: '../images/ironSword.png',
+    damageMin: 2,
+    damageMax: 4,
+    cost: 50
+  },
+  {
+    name: 'Steel Sword',
+    src: '../images/steelSword.png',
+    damageMin: 3,
+    damageMax: 6,
+    cost: 500
+  },
+  {
+    name: 'Roaas Sword',
+    src: '../images/roaasSword',
+    damageMin: 6,
+    damageMax: 10,
+    cost: ''
+  },
+  {
+    name: 'Alien Sword',
+    src: '../images/alienSword',
+    damageMin: 10,
+    damageMax: 15,
+    cost: 5000
+  },
+  {
+    name: 'Magmite Sword',
+    src: '../images/magmiteSword',
+    damageMin: 15,
+    damageMax: 25,
+    cost: ''
+  }];
     
 newWorldProject.monsterPositions = function(pos, zone) {
   var world = document.getElementById('world'),
@@ -92,36 +143,36 @@ newWorldProject.putMonstersOnScreen = function() {
 };
     
 
-newWorldProject.moveObject = function() {
-  var objectVar = {};
-  
-  function mouseDown(e) {
-    objectVar.target = e.target,
-    objectVar.xPosition = e.clientX,
-    objectVar.yPosition = e.clientY,
-    objectVar.Style = window.getComputedStyle(objectVar.target, null);
-    objectVar.xTracker = parseInt(objectVar.Style.getPropertyValue('margin-left')),
-    objectVar.yTracker = parseInt(objectVar.Style.getPropertyValue('margin-top'));
-
-    if (objectVar.target.id === 'worldMap') {
-      document.addEventListener('mousemove', mouseMove);
-    }
-    
-    document.addEventListener('mouseup', function() {
-      document.removeEventListener('mousemove', mouseMove);
-    });
-  }
-
-  function mouseMove(e) {
-    objectVar.differenceX = objectVar.xTracker - objectVar.xPosition + e.clientX,
-    objectVar.differenceY = objectVar.yTracker - objectVar.yPosition + e.clientY;
-    
-    objectVar.target.style['margin-left'] = objectVar.differenceX + 'px';
-    objectVar.target.style['margin-top'] = objectVar.differenceY + 'px';
-  }
-  
-  document.addEventListener('mousedown', mouseDown);
-};
+//newWorldProject.moveObject = function() {
+//  var objectVar = {};
+//  
+//  function mouseDown(e) {
+//    objectVar.target = e.target,
+//    objectVar.xPosition = e.clientX,
+//    objectVar.yPosition = e.clientY,
+//    objectVar.Style = window.getComputedStyle(objectVar.target, null);
+//    objectVar.xTracker = parseInt(objectVar.Style.getPropertyValue('margin-left')),
+//    objectVar.yTracker = parseInt(objectVar.Style.getPropertyValue('margin-top'));
+//
+//    if (objectVar.target.id === 'worldMap') {
+//      document.addEventListener('mousemove', mouseMove);
+//    }
+//    
+//    document.addEventListener('mouseup', function() {
+//      document.removeEventListener('mousemove', mouseMove);
+//    });
+//  }
+//
+//  function mouseMove(e) {
+//    objectVar.differenceX = objectVar.xTracker - objectVar.xPosition + e.clientX,
+//    objectVar.differenceY = objectVar.yTracker - objectVar.yPosition + e.clientY;
+//    
+//    objectVar.target.style['margin-left'] = objectVar.differenceX + 'px';
+//    objectVar.target.style['margin-top'] = objectVar.differenceY + 'px';
+//  }
+//  
+//  document.addEventListener('mousedown', mouseDown);
+//};
 
 newWorldProject.movementKeys = function() {
   var hero = document.getElementById('hero'),
@@ -188,5 +239,5 @@ newWorldProject.combatMenu = function() {
 
 window.addEventListener('load', function() {
   newWorldProject.movementKeys();
-  newWorldProject.moveObject();
+//  newWorldProject.moveObject();
 });
